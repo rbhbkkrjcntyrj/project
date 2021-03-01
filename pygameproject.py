9df5a58ab3fd9b28fddf6aec5b7a12731e5a266b
Kirill Kostenko <rbhbkkrjcntyrj@yandex.ru>
2021-03-01 03:03:29 +0300

    предфинальная попытка
    Signed-off-by: Kirill Kostenko <rbhbkkrjcntyrj@yandex.ru>

--- pygameproject.py
+++ pygameproject.py
@@ -1,7 +1,7 @@
 import pygame
 import os
 from random import randint
-
+import json
 
 pygame.init()
 height = 600
@@ -9,9 +9,9 @@ width = 1100
 screen = pygame.display.set_mode((width, height))
 all_sprites = pygame.sprite.Group()
 dino_sprite = pygame.sprite.Group()
-
+cloud_sprites = pygame.sprite.Group()
+ptero = pygame.sprite.Group()
 creator = True
-
 running_pic = [pygame.image.load(os.path.join('Изображения', 'dinosaur_run_one.png')),
                pygame.image.load(os.path.join('Изображения', 'dinosaur_run_two.png'))]
 jumping_pic = pygame.image.load(
@@ -32,7 +32,6 @@ cloud = pygame.image.load(os.path.join(
     'Изображения', 'cloud.png'))
 ground = pygame.image.load(os.path.join(
     'Изображения', 'ground.png'))
-
 speed_of_game = 14
 
 
@@ -41,9 +40,6 @@ class Game:
         self.running = True
         self.clock = pygame.time.Clock()
         self.player = Dinosaur()
-        self.cloud = Cloud()
-        #self.cactuses = [Cactus() for _ in range(3)]
-
         self.position_x_two = 0
         self.position_y_two = 380
         self.points_of_player = 0
@@ -54,7 +50,6 @@ class Game:
         self.points_of_player += 1
         if not self.points_of_player % 250:
             speed_of_game += 1
-
         text = self.font.render(
             'Счёт: ' + str(self.points_of_player), True, (0, 0, 0))
         rectangle_of_text = text.get_rect()
@@ -67,7 +62,6 @@ class Game:
         screen.blit(ground, (self.position_x_two, self.position_y_two))
         screen.blit(ground, (self.position_x_two +
                              image_width, self.position_y_two))
-
         if self.position_x_two <= -image_width:
             screen.blit(ground, (self.position_x_two +
                                  image_width, self.position_y_two))
@@ -75,36 +69,52 @@ class Game:
         self.position_x_two -= speed_of_game
 
     def play(self):
+        global speed_of_game
         fps = 30
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-
             screen.fill((255, 255, 255))
             input_of_user = pygame.key.get_pressed()
-
+            if len(cloud_sprites.sprites()) == 0:
+                for _ in range(randint(1, 4)):
+                    Cloud(cloud_sprites)
+            if len(all_sprites.sprites()) == 0:
+                for _ in range(randint(1, 3)):
+                    Cactus(all_sprites)
+            if self.points_of_player % 250 == 0 and self.points_of_player > 500:
+                Pterodactyl()
             self.player.draw(screen)
             self.player.update(input_of_user)
-
-            self.cloud.draw(screen)
-            self.cloud.update()
-
-            if len(all_sprites.sprites()) == 0:
-                for _ in range(randint(1, 4)):
-                    Cactus(all_sprites)
-            #условие на проверку столкновения
-            if pygame.sprite.spritecollideany(self.player, all_sprites):
-                self.points_of_player = 0
-                self.running = False
-
+            ptero.draw(screen)
+            ptero.update()
             all_sprites.draw(screen)
             all_sprites.update()
-
+            cloud_sprites.draw(screen)
+            cloud_sprites.update()
+            #условие на проверку столкновения
+            if pygame.sprite.spritecollideany(self.player, all_sprites) or pygame.sprite.spritecollideany(self.player, ptero):
+                speed_of_game = 0
+                with open('projectsave.json') as file:
+                    f = file.read()
+                    data = json.loads(f)
+                    for i in range(len(data["records"])):
+                        if self.points_of_player > max(data["records"]):
+                            data["records"] = [self.points_of_player] + \
+                                data["records"][:-1]
+                            break
+                        if self.points_of_player > data["records"][i]:
+                            data["records"] = data["records"][0:i] + \
+                                [self.points_of_player] + \
+                                data["records"][i:-1]
+                            break
+                    data["playerpoint"] = self.points_of_player
+                    with open('projectsave.json', "w") as file:
+                        json.dump(data, file, ensure_ascii=False)
+                self.running = False
             self.background()
-
             self.score()
-
             self.clock.tick(fps)
             pygame.display.update()
 
@@ -120,18 +130,13 @@ class Dinosaur(pygame.sprite.Sprite):
         self.duck_img = ducking_pic
         self.run_img = running_pic
         self.jump_img = jumping_pic
-
         self.dinosaur_duck = False
         self.dinosaur_run = True
         self.dinosaur_jump = False
-
         self.step_index = 0
         self.jump_val = self.jump_val_first
-        #self.sprite = pygame.sprite.Sprite()
-        #sprite = ""
         self.image = self.run_img[0]
         self.mask = pygame.mask.from_surface(self.image)
-        # rect == rect
         self.rect = self.image.get_rect()
         self.rect.x = self.position_x
         self.rect.y = self.position_y
@@ -143,27 +148,20 @@ class Dinosaur(pygame.sprite.Sprite):
             self.run()
         if self.dinosaur_jump:
             self.jump()
-
         if self.step_index >= 10:
             self.step_index = 0
-
         if event[pygame.K_UP] and not self.dinosaur_jump and self.rect.y == 310:
             self.dinosaur_duck = False
             self.dinosaur_run = False
             self.dinosaur_jump = True
-
         elif event[pygame.K_DOWN] and not self.dinosaur_jump:
             self.dinosaur_duck = True
             self.dinosaur_run = False
             self.dinosaur_jump = False
-
         elif not (self.dinosaur_jump or event[pygame.K_DOWN]):
             self.dinosaur_duck = False
             self.dinosaur_run = True
             self.dinosaur_jump = False
-
-        #if pygame.sprite.collide_mask(dino_sprite, all_sprites):
-            #game.points_of_player = 0
 
     def duck(self):
         self.image = self.duck_img[self.step_index // 5]
@@ -171,7 +169,6 @@ class Dinosaur(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = self.position_x
         self.rect.y = self.position_y_of_duck
-        #dino_sprite.add(self.sprite)
         self.step_index += 1
 
     def run(self):
@@ -180,7 +177,6 @@ class Dinosaur(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = self.position_x
         self.rect.y = self.position_y
-        #dino_sprite.add(self.sprite)
         self.step_index += 1
 
     def jump(self):
@@ -192,54 +188,19 @@ class Dinosaur(pygame.sprite.Sprite):
         if self.jump_val < - self.jump_val_first:
             self.dinosaur_jump = False
             self.jump_val = self.jump_val_first
-        #self.rect = self.image.get_rect()
-        #self.rect.x = self.position_x
-        #self.rect.y = self.position_y
-        #dino_sprite.add(self.sprite)
 
     def draw(self, screen):
         dino_sprite.add(self)
         dino_sprite.draw(screen)
-        #screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
-class Cloud:
-    def __init__(self):
-        self.position_x_cloud = width + randint(80, 100)
-        self.position_y_cloud = randint(50, 100)
-        self.image = cloud
-        self.width = self.image.get_width()
-
-    def update(self):
-        global speed_of_game
-        self.position_x_cloud -= speed_of_game
-        if self.position_x_cloud < -self.width:
-            self.position_x_cloud = width + randint(2500, 3000)
-            self.position_y_cloud = randint(50, 100)
-
-    def draw(self, screen):
-        screen.blit(self.image, (self.position_x_cloud, self.position_y_cloud))
-
-
-class Cactus(pygame.sprite.Sprite):
-    #image =
+class Cloud(pygame.sprite.Sprite):
     def __init__(self, group):
         super().__init__(group)
-        #self.sprite = pygame.sprite.Sprite(group)
-        self.image = [small_cactus_pic[randint(
-            0, 1)], big_cactus_pic[randint(0, 2)]][randint(0, 1)]
-        #Cactus.image
-        #self.sprite.image = [small_cactus_pic[randint(0, 1)], big_cactus_pic[randint(0, 2)]][randint(0, 1)]
+        self.image = cloud
         self.rect = self.image.get_rect()
-        self.rect.x = width + randint(80, 160) + \
-            240 * len(all_sprites.sprites())
-        # rect. == position_
-        if self.image in small_cactus_pic:
-            self.rect.y = 340
-        else:
-            self.rect.y = 310
-        #self.rect.y = 340
-        #self.image = [small_cactus_pic[randint(0, 1)], big_cactus_pic[randint(0, 2)]][randint(0, 1)]
+        self.rect.x = width + randint(20, 180)
+        self.rect.y = randint(30, 100)
         self.width = self.image.get_width()
         self.mask = pygame.mask.from_surface(self.image)
 
@@ -247,9 +208,53 @@ class Cactus(pygame.sprite.Sprite):
         global speed_of_game
         self.rect.x -= speed_of_game
         if self.rect.x <= -self.width:
-            #self.rect.x = width + randint(80, 100) + 100 * len(all_sprites.sprites())
             self.kill()
 
-    #def draw(self, screen):
-        #all_sprites.draw(screen)
-        #screen.blit(self.image, (self.position_x, self.position_y))
+
+class Cactus(pygame.sprite.Sprite):
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = [small_cactus_pic[randint(
+            0, 1)], big_cactus_pic[randint(0, 2)]][randint(0, 1)]
+        self.rect = self.image.get_rect()
+        self.rect.x = width + randint(480, 1080) + \
+            360 * len(all_sprites.sprites())
+        if self.image in small_cactus_pic:
+            self.rect.y = 340
+        else:
+            self.rect.y = 310
+        self.width = self.image.get_width()
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self):
+        global speed_of_game
+        self.rect.x -= speed_of_game
+        if self.rect.x <= -self.width:
+            self.kill()
+
+
+class Pterodactyl(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(ptero)
+        self.step_index = 0
+        self.image = pterodactyl[0]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.width = self.image.get_width()
+        self.rect.x = width + randint(20, 180)
+        self.rect.y = 100
+
+    def update(self):
+        global speed_of_game
+        self.image = pterodactyl[self.step_index // 5]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x -= speed_of_game
+        self.step_index += 1
+        if self.step_index >= 10:
+            self.step_index = 0
+        if self.rect.x <= -self.width:
+            self.kill()
+
+
+if __name__ == '__main__':
+    Game().play()
--- rbhbkk.py
+++ rbhbkk.py
@@ -1,37 +1,160 @@
 import pygame
 import pygameproject
 from importlib import reload
+import json
+
 
 pygame.init()
 height = 600
 width = 1100
 screenhome = pygame.display.set_mode((width, height))
 screencolor = pygame.Color("black")
+textcolor = (100, 255, 100)
+running = True
+
+
 class Startscreen:
     def __init__(self):
-        self.running = True
-        while self.running:
+        global running
+        while running:
             screenhome.fill(screencolor)
             font = pygame.font.Font(None, 50)
-            text = font.render("Добро пожаловать!", True, (100, 255, 100))
+            text = font.render("Добро пожаловать!", True, textcolor)
             text_x = width // 2 - text.get_width() // 2
             text_y = height // 8 - text.get_height() // 2
             text_w = text.get_width()
             text_h = text.get_height()
             screenhome.blit(text, (text_x, text_y))
             startbutton = font.render(
-                "Начать игру", True, (100, 255, 100))
+                "Начать игру", True, textcolor)
             startb = pygame.Rect(text_x - 10, text_y * 4 - 10,
                                  text_w + 20, text_h + 20)
             screenhome.blit(startbutton, (text_x, text_y * 4))
             pygame.draw.rect(screenhome, [0, 255, 0], startb, 1)
+            table = font.render(
+                "Таблица рекордов", True, textcolor)
+            screenhome.blit(table, (text_x, text_y * 5))
+            tableb = pygame.Rect(text_x - 10, text_y * 5 - 10,
+                                 text_w + 20, text_h + 20)
+            pygame.draw.rect(screenhome, [0, 255, 0], tableb, 1)
+            opt = font.render(
+                "Настройки", True, textcolor)
+            screenhome.blit(opt, (text_x, text_y * 6))
+            optb = pygame.Rect(text_x - 10, text_y * 6 - 10,
+                               text_w + 20, text_h + 20)
+            pygame.draw.rect(screenhome, [0, 255, 0], optb, 1)
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.running = False
+                    running = False
                 if event.type == pygame.MOUSEBUTTONDOWN and startb.collidepoint(event.pos):
                     pygameproject.Game().play()
                     reload(pygameproject)
+                    Endgame()
+                if event.type == pygame.MOUSEBUTTONDOWN and tableb.collidepoint(event.pos):
+                    RecordTable()
+                if event.type == pygame.MOUSEBUTTONDOWN and optb.collidepoint(event.pos):
+                    Options()
+
+
+class Endgame:
+    def __init__(self):
+        global running
+        while running:
+            screenhome.fill(screencolor)
+            font = pygame.font.Font(None, 50)
+            text = font.render("Игра окончена!", True, textcolor)
+            text_x = width // 2 - text.get_width() // 2
+            text_y = height // 8 - text.get_height() // 2
+            text_w = text.get_width()
+            text_h = text.get_height()
+            screenhome.blit(text, (text_x, text_y))
+            startbutton = font.render(
+                "Начать заново?", True, textcolor)
+            startb = pygame.Rect(text_x - 10, text_y * 4 - 10,
+                                 text_w + 20, text_h + 20)
+            screenhome.blit(startbutton, (text_x, text_y * 4))
+            with open('projectsave.json') as file:
+                    f = file.read()
+                    data = json.loads(f)
+            shbutton = font.render(
+                "Ваш счёт: " + str(data["playerpoint"]), True, textcolor)
+            screenhome.blit(shbutton, (text_x, text_y * 2))
+            pygame.draw.rect(screenhome, [0, 255, 0], startb, 1)
+            table = font.render(
+                "К началу", True, textcolor)
+            screenhome.blit(table, (text_x, text_y * 5))
+            tableb = pygame.Rect(text_x - 10, text_y * 5 - 10,
+                                 text_w + 20, text_h + 20)
+            pygame.draw.rect(screenhome, [0, 255, 0], tableb, 1)
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN and startb.collidepoint(event.pos):
+                    pygameproject.Game().play()
+                    reload(pygameproject)
+                    Endgame()
+                if event.type == pygame.MOUSEBUTTONDOWN and tableb.collidepoint(event.pos):
+                    Startscreen()
+
+
+class RecordTable:
+    def __init__(self):
+        global running
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN and tableb.collidepoint(event.pos):
+                    Startscreen()
+            screenhome.fill(screencolor)
+            font = pygame.font.Font(None, 50)
+            text = font.render(
+                "Таблица рекордов", True, textcolor)
+            text_x = width // 2 - text.get_width() // 2
+            text_y = height // 8 - text.get_height() // 2
+            text_w = text.get_width()
+            text_h = text.get_height()
+            with open('projectsave.json') as file:
+                    f = file.read()
+                    data = json.loads(f)
+                    for i in range(1, 9):
+                        text = font.render(str(i) + ". " +
+                                           str(data["records"][i - 1]), True, textcolor)
+                        screenhome.blit(text, (text_x, text_y * i))
+            table = font.render("К началу", True, textcolor)
+            screenhome.blit(table, (text_x, text_y * 10 - 50))
+            tableb = pygame.Rect(text_x - 10, text_y * 10 - 60,
+                                 text_w + 20, text_h + 20)
+            pygame.draw.rect(screenhome, [0, 255, 0], tableb, 1)
+            pygame.display.flip()
+
+
+class Options:
+    def __init__(self):
+        global running
+        while running:
+            screenhome.fill(screencolor)
+            font = pygame.font.Font(None, 50)
+            text = font.render(
+                "Таблица рекордов", True, textcolor)
+            text_x = width // 2 - text.get_width() // 2
+            text_y = height // 8 - text.get_height() // 2
+            text_w = text.get_width()
+            text_h = text.get_height()
+            table = font.render("К началу", True, textcolor)
+            screenhome.blit(table, (text_x, text_y * 10 - 50))
+            tableb = pygame.Rect(text_x - 10, text_y * 10 - 60,
+                                 text_w + 20, text_h + 20)
+            pygame.draw.rect(screenhome, [0, 255, 0], tableb, 1)
+            for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                    if event.type == pygame.MOUSEBUTTONDOWN and tableb.collidepoint(event.pos):
+                        Startscreen()
+            pygame.display.flip()
+
 
 if __name__ == '__main__':
     Startscreen()
